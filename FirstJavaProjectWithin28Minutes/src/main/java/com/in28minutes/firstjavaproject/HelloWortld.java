9e2112df6d81d788507ea948815d7711fadbd28d
Home-Daniel <wei.liang.hong84@gmail.com>
2023-12-21 06:35:07 +0800

    2023-12-21  77. Step 03 - Writing Multiplication Table Java Program with Eclipse

--- FirstJavaProjectWithin28Minutes/src/main/java/com/in28minutes/firstjavaproject/HelloWortld.java
+++ FirstJavaProjectWithin28Minutes/src/main/java/com/in28minutes/firstjavaproject/HelloWortld.java
@@ -1,6 +1,6 @@
 package com.in28minutes.firstjavaproject;
 
-public class HellWortld {
+public class HelloWortld {
     public static void main(String[] args){
         System.out.println("Hello World");
     }
