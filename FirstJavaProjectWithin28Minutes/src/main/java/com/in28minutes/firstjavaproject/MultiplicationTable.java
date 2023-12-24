a1b34aec388f4f6649f355ed7c164aa7cecd0d0f
Home-Daniel <wei.liang.hong84@gmail.com>
2023-12-25 06:05:57 +0800

    2023-12-25  81. Step 05 - Programming Tip 1 : Refactoring with Eclipse

--- FirstJavaProjectWithin28Minutes/src/main/java/com/in28minutes/firstjavaproject/MultiplicationTable.java
+++ FirstJavaProjectWithin28Minutes/src/main/java/com/in28minutes/firstjavaproject/MultiplicationTable.java
@@ -4,15 +4,11 @@ public class MultiplicationTable {
     // 5*1 = 5
     // 5*10 = 50
     void print(){
-        for(int i = 1; i<=10; i++){
-            System.out.printf("%d * %d = %d", 5, i, 5 * i).println();
-        }
+        print(5);
     }
 
     void print(int table){
-        for(int i = 1; i<=10; i++){
-            System.out.printf("%d * %d = %d", table, i, table * i).println();
-        }
+        print(table, 1, 10);
     }
 
     void print(int table, int from, int to){
--- FirstJavaProjectWithin28Minutes/src/main/java/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
+++ FirstJavaProjectWithin28Minutes/src/main/java/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -3,8 +3,8 @@ package com.in28minutes.firstjavaproject;
 public class MultiplicationTableRunner {
     public static void main(String[] args){
         MultiplicationTable table = new MultiplicationTable();
-        //table.print();
+        table.print();
         //table.print(6);
-        table.print(6, 11, 20);
+        //table.print(6, 11, 20);
     }
 }
