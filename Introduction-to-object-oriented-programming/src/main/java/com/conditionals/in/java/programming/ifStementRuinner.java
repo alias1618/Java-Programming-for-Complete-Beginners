25cb5bfe5ded3c1ad99e4b14ea705370cbd7a841
Home-Daniel <wei.liang.hong84@gmail.com>
2024-01-24 07:10:16 +0800

    2024-01-24  142. Step 04 - If Else Problem - How to get User Input in Java?

--- Introduction-to-object-oriented-programming/src/main/java/com/conditionals/in/java/programming/ifStementRuinner.java
+++ Introduction-to-object-oriented-programming/src/main/java/com/conditionals/in/java/programming/ifStementRuinner.java
@@ -3,6 +3,14 @@ package com.conditionals.in.java.programming;
 public class ifStementRuinner {
 
     public static void main(String[] args) {
+       // nestedIfElse();
+        //puzzle1();
+        //puzzle2();
+        //puzzle3();
+        puzzle5();
+    }
+
+    public static void nestedIfElse(){
         int i = 25;
         if(i == 25){
             System.out.println("i == 25");
@@ -11,5 +19,50 @@ public class ifStementRuinner {
         }else{
             System.out.println("i != 24 and i != 25");
         }
+    }
+    private static void puzzle1(){
+        int k = 15;
+        if(k > 20){
+            System.out.println(1);
+        } else if (k > 10) {
+            System.out.println(2);
+        } else if (k < 20) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+        }
+    }
+
+    private static void puzzle2(){
+        int l = 15;
+
+        if(l < 20){
+            System.out.println("l < 20");
+        }
+        if (l > 20){
+            System.out.println("l > 20");
+        }else{
+            System.out.println("Who am I");
+        }
+    }
+
+    private static void puzzle3(){
+        int m = 15;
+        if(m > 20){
+            if(m<20){
+                System.out.println("m > 20");
+            }else {
+                System.out.println("Who am I");
+            }
+        }
+    }
+
+    private static void puzzle5(){
+        int number = 5;
+        if(number < 0){
+            number = number + 10;
+        }
+        number++;
+        System.out.println(number);
     }
 }
