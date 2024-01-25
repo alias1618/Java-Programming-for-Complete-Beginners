b27824d2e4f79e28077fdcdeb82ce365d419740f
Home-Daniel <wei.liang.hong84@gmail.com>
2024-01-26 06:15:25 +0800

    2024-01-26  148. Step 08 - Java Switch Statement - Puzzles - Default, Break and Fall Through

---
+++ Introduction-to-object-oriented-programming/src/main/java/com/conditionals/in/java/programming/step08/Java/Switch/Statement/Puzzles/PuzzlesRunner.java
@@ -0,0 +1,13 @@
+package com.conditionals.in.java.programming.step08.Java.Switch.Statement.Puzzles;
+
+public class PuzzlesRunner {
+    public static void main(String[] args) {
+        Puzzles puzzles = new Puzzles();
+        //puzzles.puzzle1();
+        //puzzles.puzzle2();
+        //puzzles.puzzle3();
+        //puzzles.puzzle4();
+        //puzzles.puzzle5();
+        puzzles.puzzle6();
+    }
+}
