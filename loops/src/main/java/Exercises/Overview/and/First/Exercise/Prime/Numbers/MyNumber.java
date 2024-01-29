6e8d905deee65cdc4e49f278c694807611d5e675
Home-Daniel <wei.liang.hong84@gmail.com>
2024-01-30 06:22:51 +0800

    2024-01-30  161. Step 04 - Java For Loop - Exercise - Print a Number Triangle

--- loops/src/main/java/Exercises/Overview/and/First/Exercise/Prime/Numbers/MyNumber.java
+++ loops/src/main/java/Exercises/Overview/and/First/Exercise/Prime/Numbers/MyNumber.java
@@ -43,4 +43,19 @@ public class MyNumber {
         }
         return sum;
     }
+
+    public void printNumberTriangle() {
+        //1
+        //1 2
+        //1 2 3
+        //1 2 3 4
+        //1 2 3 4 5
+
+        for(int i = 1; i<= number; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- loops/src/main/java/Exercises/Overview/and/First/Exercise/Prime/Numbers/MyNumberRunner.java
+++ loops/src/main/java/Exercises/Overview/and/First/Exercise/Prime/Numbers/MyNumberRunner.java
@@ -2,7 +2,7 @@ package Exercises.Overview.and.First.Exercise.Prime.Numbers;
 
 public class MyNumberRunner {
     public static void main(String[] args) {
-        MyNumber number = new MyNumber(6);
+        MyNumber number = new MyNumber(5);
         boolean isPrime = number.isPrime();
         System.out.println("isPrime " + isPrime);
 
@@ -11,5 +11,8 @@ public class MyNumberRunner {
 
         int sumOfDivisors = number.sumOfDivisors();
         System.out.println("sumOfDivisors " + sumOfDivisors);
+
+        number.printNumberTriangle();
+        //System.out.println("printNumberTriangle " + printNumberTriangle);
     }
 }
