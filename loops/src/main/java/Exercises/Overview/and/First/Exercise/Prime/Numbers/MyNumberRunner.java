d9f2528d15b05f9c3602b6c6748e3cd74d2d4c5c
Home-Daniel <wei.liang.hong84@gmail.com>
2024-01-30 06:04:31 +0800

    2024-01-30   160. Step 03 - Java For Loop - Exercise - Sum Upto N Numbers and Sum of Divisors

---
+++ loops/src/main/java/Exercises/Overview/and/First/Exercise/Prime/Numbers/MyNumberRunner.java
@@ -0,0 +1,15 @@
+package Exercises.Overview.and.First.Exercise.Prime.Numbers;
+
+public class MyNumberRunner {
+    public static void main(String[] args) {
+        MyNumber number = new MyNumber(6);
+        boolean isPrime = number.isPrime();
+        System.out.println("isPrime " + isPrime);
+
+        int sum = number.sumUptoN();
+        System.out.println("sumUptoN " + sum);
+
+        int sumOfDivisors = number.sumOfDivisors();
+        System.out.println("sumOfDivisors " + sumOfDivisors);
+    }
+}
