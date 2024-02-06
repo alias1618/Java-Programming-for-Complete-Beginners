0249a8718c4c4eaea269890fcda8e2a74780bff3
Home-Daniel <wei.liang.hong84@gmail.com>
2024-02-07 06:41:04 +0800

    2024-02-07  164. Step 06 - While Loop - Exericises - Cubes and Squares upto limit

---
+++ loops/src/main/java/Exercises/Overview/and/First/Exercise/Prime/Numbers/WhileNumberPlayerRunner.java
@@ -0,0 +1,11 @@
+package Exercises.Overview.and.First.Exercise.Prime.Numbers;
+
+public class WhileNumberPlayerRunner {
+    public static void main(String[] args) {
+        WhileNumberPlayer player = new WhileNumberPlayer(30);
+
+        player.printSquarerUptoLimit();
+
+        player.printCubesUptoLimit();
+    }
+}
