0ea6796f910ec02ca4e32605d5ab95f32f75c8f2
Home-Daniel <wei.liang.hong84@gmail.com>
2024-06-07 06:54:17 +0800

    2024-06-07 212. Step 11 - Java String Arrays - Exercise Solutions - Print Day of Week with Most

---
+++ Arrays_and_ArrayLists_in_Java_Programming/src/main/java/com/in28minutes/arrays/_212/Step11/StringRunner.java
@@ -0,0 +1,26 @@
+package com.in28minutes.arrays._212.Step11;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String[] dayOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+        String dayWithMostCharacters = "";
+        for(String day:dayOfWeek){
+            if(day.length() > dayWithMostCharacters.length()){
+                dayWithMostCharacters = day;
+            }
+        }
+
+        System.out.println("Day with Most number of characters " + dayWithMostCharacters);
+
+        //正向印出
+        for(int i = 0; i<dayOfWeek.length;i++){
+            System.out.println(dayOfWeek[i]);
+        }
+
+        //反向印出
+        for(int i = dayOfWeek.length - 1; i >= 0 ;i--){
+            System.out.println(dayOfWeek[i]);
+        }
+    }
+}
