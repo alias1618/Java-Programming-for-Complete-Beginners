83deb9d41e0cad551ef8ad02f9619a1449b3ef65
Home-Daniel <wei.liang.hong84@gmail.com>
2024-07-05 06:41:16 +0800

    2024-07-05  237. Step 07 - Understanding Inheritance - Why do we need it?

---
+++ object-oriented-programming-2/src/main/java/com/in28minutes/oop/level2/inheritance/StudentRunner.java
@@ -0,0 +1,9 @@
+package com.in28minutes.oop.level2.inheritance;
+
+public class StudentRunner {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Ranga");
+        student.setEmail("in28minutes@gmail.com");
+    }
+}
