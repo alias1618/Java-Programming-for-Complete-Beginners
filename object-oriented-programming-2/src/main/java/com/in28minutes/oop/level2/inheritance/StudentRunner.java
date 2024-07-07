4d4303b0d31277a7739eda04e7367a80cb9640da
Home-Daniel <wei.liang.hong84@gmail.com>
2024-07-08 06:34:49 +0800

    2024-07-08  240. Step 10 - Java Inheritance - Exercise - Student and Employee Classes

--- object-oriented-programming-2/src/main/java/com/in28minutes/oop/level2/inheritance/StudentRunner.java
+++ object-oriented-programming-2/src/main/java/com/in28minutes/oop/level2/inheritance/StudentRunner.java
@@ -2,8 +2,17 @@ package com.in28minutes.oop.level2.inheritance;
 
 public class StudentRunner {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setName("Ranga");
-        student.setEmail("in28minutes@gmail.com");
+//        Student student = new Student();
+//        student.setName("Ranga");
+//        student.setEmail("in28minutes@gmail.com");
+
+        Employee employee = new Employee();
+        employee.setName("Ranga");
+        employee.setEmail("Ranga@in28minutes.com");
+        employee.setPhoneNumber("123-456-7890");
+        employee.setEmployeeGrade('A');
+        employee.setTitle("Programmer Analyst");
+
+        System.out.println(employee);
     }
 }
